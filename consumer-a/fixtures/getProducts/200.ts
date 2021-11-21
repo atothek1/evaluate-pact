01b5b60f80c461669a6031e7fa7e5653a6d2bf56
Alexander Kämmerer <atothek1@gmail.com>
2021-11-21 11:01:21 +0100

    init commit

---
+++ consumer-a/fixtures/getProducts/200.ts
@@ -0,0 +1,33 @@
+const faker = require("faker")
+import { Product } from "../../src/types";
+
+const {commerce, datatype, image } = faker;
+faker.seed(1234);
+
+function createImageUrls(): string [] {
+    const res: string[] = [];
+    for (let i = 0; i < 10; i++) {
+        res.push(image.imageUrl());
+    }
+    return res
+}
+
+function createProduct(): Product {
+    return {
+        id: datatype.uuid(),
+        name: commerce.productName(),
+        price: parseFloat(commerce.price()) * 10,
+        isAvailable: datatype.boolean(),
+        imageUrls: createImageUrls()
+    }
+}
+
+function createProducts(): Product [] {
+    const res: Product[] = [];
+    for (let i = 0; i < 10; i++) {
+        res.push(createProduct());
+    }
+    return res
+}
+
+export const products = createProducts();
